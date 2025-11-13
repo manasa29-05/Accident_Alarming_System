6e6ebfb9a1d67e91c0eb2a1013856ee1463c77ed
K.SAI MANASA <manasa290505@gmail.com>
2025-11-13 11:18:50 +0530

    Update main.py to link all modules

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Accident Alarming System is running successfully!")
+from sensor_reader import simulate_reading
+from detector import detect_accident
+from notifier import send_alert
+import time
+
+while True:
+    ax, ay, az = simulate_reading()
+    if detect_accident(ax, ay, az):
+        send_alert()
+    else:
+        print(f"All normal... ax={ax:.2f}, ay={ay:.2f}, az={az:.2f}")
+    time.sleep(2)
+
